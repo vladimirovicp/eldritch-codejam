d4897e0c46356a0a663abfdf01ed6456fb865b1e
vladimirovicp <vladimirovicp70@gmail.com>
2022-08-23 21:55:49 +0400

    feat: generate maket

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,11 @@ import difficulties from '../../data/difficulties.js';  // Уровни слож
 import ancients from '../../data/ancients.js';
 
 const ancientsContainer = document.querySelector('.ancients');
+const difficultyContainer = document.querySelector('.difficulty');
+const deckContainer = document.querySelector('.deck');
+
+let difficultyCurrent;
+let deckBtn;
 
 // console.log(difficulties);
 // console.log(ancients);
@@ -34,6 +39,67 @@ if (ancientsCard.length > 0){
 function ancientsCardClick(e){
     const targetAncients = e.currentTarget;
     const idAncients = targetAncients.dataset.id;
+    // console.log(idAncients);
 
-    console.log(idAncients);
+    difficulty();
 }
+
+
+function difficulty(){
+    difficultyContainer.innerHTML = '';
+    difficulties.forEach(el =>{
+        console.log(el);
+        let div = document.createElement('div');
+        div.className = "difficulty__item";
+        div.dataset.id = el.id;
+        div.textContent = el.name;
+        difficultyContainer.appendChild(div);
+    });
+
+    difficultyCurrent = difficultyContainer.querySelectorAll('.difficulty__item[data-id]');
+    if (difficultyCurrent.length > 0){
+        difficultyCurrent.forEach( difficultySelected => {
+            difficultySelected.addEventListener("click",  difficultyCardClick);
+        });
+    }
+
+}
+
+difficulty()
+
+
+function difficultyCardClick(e){
+    const targetDifficulty = e.currentTarget;
+    const idDifficulty = targetDifficulty.dataset.id;
+    difficultyCurrent.forEach(el =>{
+        el.dataset.id === idDifficulty ? el.classList.add('active') : el.classList.remove('active');
+    });
+    console.log(idDifficulty);
+
+    deck();
+}
+
+
+function deck(){
+    deckContainer.innerHTML = '';
+    let span = document.createElement('span');
+    span.className = "shuffle-button";
+    span.textContent =' Замешать колоду';
+    deckContainer.appendChild(span);
+
+    deckBtn = deckContainer.querySelector('.shuffle-button');
+    deckBtn.addEventListener('click', deckClick);
+}
+
+deck();
+
+function deckClick(){
+    console.log('клик')
+}
+
+
+
+
+
+
+
